a5a8407cacbf954b021255776811c96703e91aca
OJai <omarjarjour.lb@gmail.com>
2024-07-25 06:40:01 +0300

    yes

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-    const cursor = document.querySelector('.cursor');
-
-    // Position the cursor element centered at the mouse pointer
-    cursor.style.top = `${e.clientY}px`;
-    cursor.style.left = `${e.clientX}px`;
-    cursor.style.transform = 'translate(-50%, -50%)';
-});
