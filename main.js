07313f80ccfbaf07a98efddadb6fd001ea7e3fcd
OJai <omarjarjour.lb@gmail.com>
2024-07-26 19:22:50 +0300

    main done

--- main.js
+++ main.js
@@ -36,3 +36,32 @@ const blurHeader = () =>{
     this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
+
+/* Scroll up */
+
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+/* scroll section active link */
+
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
