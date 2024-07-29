35e5b42fe03b215e86a5d87feb7ec9f0e0be0a26
OJai <omarjarjour.lb@gmail.com>
2024-07-29 23:07:09 +0300

    nav close was bugging with new and default cursor

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ document.addEventListener('mouseout', () => {
 
 
 // Enlarge the cursor on hover and reset on click
-const clickableElements = document.querySelectorAll('a, button, .clickable-element, i, input, textarea'); // Add other elements as needed
+const clickableElements = document.querySelectorAll('a, button, .clickable-element, i, input, textarea, nav__toggle, nav__close'); // Add other elements as needed
 
 clickableElements.forEach(el => {
     el.addEventListener('mouseenter', () => cursor.classList.add('enlarged'));
@@ -108,7 +108,7 @@ clickableElements.forEach(el => {
 document.addEventListener('click', () => {
     cursor.classList.remove('enlarged');
     setTimeout(() => {
-        const el = document.querySelector('a:hover, button:hover, .clickable-element:hover, i:hover, input:hover, textarea:hover');
+        const el = document.querySelector('a:hover, button:hover, .clickable-element:hover, i:hover, input:hover, textarea:hover, .nav__toggle:hover, .nav__close:hover');
         if (el) {
             cursor.classList.add('enlarged');
         }
